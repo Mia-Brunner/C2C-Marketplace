4a9e838db3e3b93bf13ae6aa415fee8639e44bc0
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-03 17:07:57 +1100

    Created View Indexfor Listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,43 @@
+class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show,:edit,:update,:destroy]
+
+  def index
+    @listings = Listing.all
+  end
+  
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if(@listing.save)
+      redirect_to @listing
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :breed, :price, :documentation, :description)
+  end 
+
+  def find_listing
+    @listing = Listing.find(params[:id])
+  end
+
+end
