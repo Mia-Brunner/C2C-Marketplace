c7318ab0f140973b92e819b65e8fbdd4d5e1df32
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-05 01:55:12 +1100

    image magick force resize to 300 width

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  def thumbnail input
+    return self.images[input].variant(resize: '300x300').processed
+  end
 end
