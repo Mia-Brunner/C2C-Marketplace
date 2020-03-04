6c6c94262e445de5105d4fd89b139a0e935f1f82
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-05 00:24:42 +1100

    Added Phone Number Column to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_085241) do
+ActiveRecord::Schema.define(version: 2020_03_04_131850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_085241) do
     t.string "lastname"
     t.string "location"
     t.string "aboutme"
+    t.integer "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
