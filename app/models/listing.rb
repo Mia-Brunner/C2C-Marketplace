89b2251825427d7589210cc644c01e0a174589ea
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-10 17:15:07 +1100

    Changing Seeds

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,21 +4,10 @@ class Listing < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true
   validates :price, presence: true
-  validate :image_type
 
   def thumbnail input
     return self.images[input].variant(resize: '300x300').processed
   end
 
-  private
-  def image_type
-    if images.attached? == false
-      errors.add(:images, "are missing!")
-    end
-    images.each do |image|
-      if !image.content_type.in?(%('image/jpeg image/png'))
-        errors.add(:images, 'needs to be a JPEG or PNG')
-      end
-    end
-  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,26 @@
-users =[
-  user1 = User.create(email: 'test1@dog.com', password: 'password'),
-  user2 = User.create(email: 'test3@dog.com', password: 'password')
-]
 
-users.each do |user|
-  2.times do
-    Listing.create(
-      name: 'Charlie',
-      breed: 'Corgi',
-      price: 1000.00,
-      documentation: 'Vaccinated',
-      description: 'Dog',
-      user_id: user1.id
-    )
-  end
 
-  2.times do
-    Listing.create(
-      name: 'Peanut',
-      breed: 'Yes',
-      price: 1000.00,
-      documentation: 'Vaccinated',
-      description: 'Dog',
-      user_id: user2.id
-    )
-  end
-end
+admin = User.create(
+  email: 'mia@admin.com',
+  password: 'admin1',
+  firstname: 'Mia',
+  lastname: 'Brunner',
+  location: 'Melbourne',
+  phone: '0411 999 111',
+)
+
+admin.save
+puts 'Default User Created'
+puts 'Email: mia@admin.com'
+puts 'Password: admin1'
+
+
+listing_1 = Listing.create(
+  name: 'Charlie',
+  breed: 'Corgi',
+  price: 2000,
+  documentation: 'Microchip Vaccinated, Desexed',
+  description: 'Sable, 6 months',
+  user_id: '1'
+)
+listing_1.save
