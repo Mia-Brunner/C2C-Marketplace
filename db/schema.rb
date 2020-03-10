7001234d5b602ee07d465730a0aa04de2977236a
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-10 17:17:10 +1100

    Change Schema phone from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_142636) do
+ActiveRecord::Schema.define(version: 2020_03_10_061533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_142636) do
     t.string "lastname"
     t.string "location"
     t.string "aboutme"
-    t.integer "phone"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
